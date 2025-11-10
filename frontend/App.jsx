e872cb43efafa37146333acef08e7316d15fbaaf
Santhosh1204 <santhoshkumara1204@gmail.com>
2025-11-10 10:47:54 +0530

    fix: routing for vercel using HashRouter

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,6 +1,8 @@
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import ChatbotWidget from "./ChatbotWidget";
 import Welcome from "./Welcome";
 import LanguageSelection from "./LanguageSelection";
