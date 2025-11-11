1919143863b66d3da129cb1ddaf23bc4df614841
Santhosh1204 <santhoshkumara1204@gmail.com>
2025-11-11 20:12:52 +0530

    fix: added vercel.json rewrite for SPA routing

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,14 +1,10 @@
-
 import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import ChatbotWidget from "./ChatbotWidget";
 import Welcome from "./Welcome";
 import LanguageSelection from "./LanguageSelection";
-
 import Feedback from "./Feedback";
-
 
 function App() {
   return (
@@ -16,16 +12,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/language" element={<LanguageSelection />} />
-
         <Route path="/chat" element={<ChatbotWidget />} />
         <Route path="/feedback" element={<Feedback />} />
-
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
-
